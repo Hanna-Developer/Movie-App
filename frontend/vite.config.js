3d9be090558106a7b9511aca1cc7cfdfd77251b5
Hanna-Developer <168489219+Hanna-Developer@users.noreply.github.com>
2024-09-23 22:57:52 +0300

    Updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,14 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     // dev
-    // proxy: {
-    //   "/api/": "http://localhost:3000",
-    //   "/uploads/": "http://localhost:3000",
-    // },
-    // prod
     proxy: {
-      "/api/": "https://movie-app-64mh.onrender.com",
-      "/uploads/": "https://movie-app-64mh.onrender.com",
+      "/api/": "http://localhost:3000",
+      "/uploads/": "http://localhost:3000",
     },
   },
 });
