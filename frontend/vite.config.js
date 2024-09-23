e8bdd838ba849f8ae689bf73b737b3be52a397ba
Hanna-Developer <168489219+Hanna-Developer@users.noreply.github.com>
2024-09-23 17:09:48 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     // dev
-    proxy: {
-      "/api/": "http://localhost:3000",
-      "/uploads/": "http://localhost:3000",
-    },
+    // proxy: {
+    //   "/api/": "http://localhost:3000",
+    //   "/uploads/": "http://localhost:3000",
+    // },
     // prod
     proxy: {
       "/api/": "https://movie-app-64mh.onrender.com",
